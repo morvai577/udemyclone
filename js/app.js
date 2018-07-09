9f2e56b3294babf6f6fcbe26a2764bcee81a759c
Vaibhav Mor <mor.vaibhav577@gmail.com>
2018-07-09 22:02:25 +1200

    Add code to save shopping cart to local storage

--- js/app.js
+++ js/app.js
@@ -17,6 +17,9 @@ function loadEventListeners() {
 
     // Clear Cart Btn
     clearCartBtn.addEventListener('click', clearCart);
+
+    // Document Ready
+    document.addEventListener('DOMContentLoaded', getFromLocalStorage);
 }
 
 // Functions
@@ -70,6 +73,36 @@ function addIntoCart(course) {
 
     // Add into the shopping cart
     shoppingCartContent.appendChild(row);
+
+    // Add course into local storage
+    saveIntoStorage(course);
+}
+
+// Add the courses into the local storage
+function saveIntoStorage(course) {
+
+    let courses = getCourseFromStorage();
+
+    // add the course into the array
+    courses.push(course);
+
+    // since storage only saves strings, we need to convert JSON into string here
+    localStorage.setItem('courses', JSON.stringify(courses))
+}
+
+// Get the contents from storage
+function getCourseFromStorage() {
+
+    let courses;
+
+    // if something exist on storage then get the value, otherwise create an empty array
+    if(localStorage.getItem('courses') === null) {
+        courses = [];
+    } else {
+        courses = JSON.parse(localStorage.getItem('courses'));
+    }
+
+    return courses;
 }
 
 // Remove course from the dom
@@ -86,4 +119,30 @@ function clearCart() {
     while(shoppingCartContent.firstChild) {
         shoppingCartContent.removeChild(shoppingCartContent.firstChild);
     }
+}
+
+// Loads when doc is ready and prints courses into shopping cart
+function getFromLocalStorage() {
+    let coursesLS = getCourseFromStorage();
+
+    // LOOP through coursesLS and print into cart
+    coursesLS.forEach(function(course){
+        // Create <tr>
+        const row = document.createElement('tr');
+
+        // print the content
+        row.innerHTML = `
+            <tr>
+                <td>
+                    <img src="${course.image}" width=100px>
+                </td>
+                <td>${course.title}</td>
+                <td>${course.price}</td>
+                <td>
+                    <a href="#" class="remove" data-id="${course.id}">X</a>
+                </td>
+            </tr>
+        `;
+        shoppingCartContent.appendChild(row);
+    });
 }
